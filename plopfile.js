aee39543ac6ed4eed04cfefab2bb9cdd0ff19ef1
Fabricio Borgobello <fborgobello@gmail.com>
2023-11-07 15:03:38 +0100

    Add PLOP (second iteration)

--- plopfile.js
+++ plopfile.js
@@ -8,12 +8,28 @@ export default function (plop) {
         message: 'What is your resource name?',
       },
     ],
-    actions: [
-      {
-        type: 'add',
-        path: 'src/{{name}}/{{name}}.controller.ts',
-        templateFile: 'plop-templates/controller.ts.hbs',
-      },
-    ],
+    actions: function () {
+      const actions = [
+        {
+          type: 'add',
+          path: 'src/{{name}}/{{name}}.controller.ts',
+          templateFile: 'plop-templates/controller.ts.hbs',
+        },
+        {
+          type: 'modify',
+          path: 'src/routes.ts',
+          pattern: /(import.*from.*;\n\nconst router = new Hono\(\);)/g,
+          template: `import {{camelCase name}}Router from '@/{{name}}/{{name}}.controller';\n$1`,
+        },
+        {
+          type: 'modify',
+          path: 'src/routes.ts',
+          pattern: /(\/\/ Add routes here)/g,
+          template: `$1\nrouter.route('/{{dashCase name}}', {{camelCase name}}Router);`,
+        },
+      ];
+
+      return actions;
+    },
   });
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono';
 
 import resourceRouter from '@/resource/resource.controller';
+import usersRouter from '@/users/users.controller';
 
 const router = new Hono();
 
 // Add routes here
+router.route('/users', usersRouter);
 router.route('/resource', resourceRouter);
 
 export default router;
