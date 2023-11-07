43e1e5fd26584a62d83e2db825844c2484e89bad
Fabricio Borgobello <fborgobello@gmail.com>
2023-11-07 13:36:50 +0100

    Add middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { wiseRouter } from './wise';
+import { prettyJSON } from 'hono/pretty-json';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
+import { secureHeaders } from 'hono/secure-headers';
 
 const app = new Hono();
+
+// Middlewares
+app.use('*', prettyJSON()); // Add ?pretty to the URL to get pretty JSON
+app.use('*', cors());
+app.use('*', logger());
+app.use('*', secureHeaders());
 
 // Routes
 app.route('/wise', wiseRouter);
